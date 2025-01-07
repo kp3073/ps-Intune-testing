b1db1a035b5a6af475c6a5c9aa8be80b7880f335
kp3073 <keyurpatel8288@gmail.com>
2025-01-07 12:11:08 -0500

    testps

--- register.ps1
+++ register.ps1
@@ -1,25 +1,25 @@
-# Define variables
-$vaultName = "IntuneSecretsaz"
-$secretName = "clientSecret"
-$tenantIdName = "tenantId"
-$clientIdName = "clientId"
-$grouptag = "XYZ"
-
-# Retrieve secrets from Azure Key Vault
-$clientSecret = (Get-AzKeyVaultSecret -VaultName $vaultName -Name $secretName).SecretValueText
-$tenantId = (Get-AzKeyVaultSecret -VaultName $vaultName -Name $tenantIdName).SecretValueText
-$clientId = (Get-AzKeyVaultSecret -VaultName $vaultName -Name $clientIdName).SecretValueText
-
-# Get an access token
-$body = @{
-	grant_type = "client_credentials"
-	client_id = $clientId
-	client_secret = $clientSecret
-	scope = "https://graph.microsoft.com/.default"
-}
-
-$response = Invoke-RestMethod -Method Post -Uri "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token" -ContentType "application/x-www-form-urlencoded" -Body $body
-$accessToken = $response.access_token
+## Define variables
+#$vaultName = "IntuneSecretsaz"
+#$secretName = "clientSecret"
+#$tenantIdName = "tenantId"
+#$clientIdName = "clientId"
+#$grouptag = "XYZ"
+#
+## Retrieve secrets from Azure Key Vault
+#$clientSecret = (Get-AzKeyVaultSecret -VaultName $vaultName -Name $secretName).SecretValueText
+#$tenantId = (Get-AzKeyVaultSecret -VaultName $vaultName -Name $tenantIdName).SecretValueText
+#$clientId = (Get-AzKeyVaultSecret -VaultName $vaultName -Name $clientIdName).SecretValueText
+#
+## Get an access token
+#$body = @{
+#	grant_type = "client_credentials"
+#	client_id = $clientId
+#	client_secret = $clientSecret
+#	scope = "https://graph.microsoft.com/.default"
+#}
+#
+#$response = Invoke-RestMethod -Method Post -Uri "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token" -ContentType "application/x-www-form-urlencoded" -Body $body
+#$accessToken = $response.access_token
 
 # Install the required module
 Install-Script -Name Get-WindowsAutopilotInfo -Force
@@ -28,4 +28,4 @@ Install-Script -Name Get-WindowsAutopilotInfo -Force
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
 
 # Run the Autopilot script
-Get-WindowsAutopilotInfo -GroupTag $grouptag -Online -AccessToken $accessToken
+Get-WindowsAutopilotInfo -GroupTag $grouptag -Online 

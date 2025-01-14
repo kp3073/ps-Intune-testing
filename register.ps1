a8ebb748bc813feda7a121ddd3634ddc2e7572e8
Keyur Patel <143982764+kp3073@users.noreply.github.com>
2025-01-14 15:04:45 -0500

    Newregister.ps1

--- register.ps1
+++ register.ps1
@@ -1,31 +1,27 @@
-## Define variables
-#$vaultName = "IntuneSecretsaz"
-#$secretName = "clientSecret"
-#$tenantIdName = "tenantId"
-#$clientIdName = "clientId"
-#$grouptag = "XYZ"
-#
-## Retrieve secrets from Azure Key Vault
-#$clientSecret = (Get-AzKeyVaultSecret -VaultName $vaultName -Name $secretName).SecretValueText
-#$tenantId = (Get-AzKeyVaultSecret -VaultName $vaultName -Name $tenantIdName).SecretValueText
-#$clientId = (Get-AzKeyVaultSecret -VaultName $vaultName -Name $clientIdName).SecretValueText
-#
-## Get an access token
-#$body = @{
-#	grant_type = "client_credentials"
-#	client_id = $clientId
-#	client_secret = $clientSecret
-#	scope = "https://graph.microsoft.com/.default"
-#}
-#
-#$response = Invoke-RestMethod -Method Post -Uri "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token" -ContentType "application/x-www-form-urlencoded" -Body $body
-#$accessToken = $response.access_token
+# Variables
+$AppID = "abdcc16f-2ff2-49b4-beb2-b7bdb521bcfb"
+$tenantId = "a221016b-cc73-49c9-a858-7fca1f010971"
+$AppSecret = "Z7j8Q~66-.lp7LkQcCuMNMuE0vn0n3TEIh0mZbUC"
+$groupTag = "SJC"
 
-# Install the required module
+# Install the Get-WindowsAutopilotInfo script
 Install-Script -Name Get-WindowsAutopilotInfo -Force
 
-# Set execution policy
+# Set execution policy to allow running the script
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
 
-# Run the Autopilot script
-Get-WindowsAutopilotInfo -GroupTag $grouptag -Online 
+# Authenticate to Microsoft Graph
+$body = @{
+    grant_type    = "client_credentials"
+    scope         = "https://graph.microsoft.com/.default"
+    client_id     = $AppID
+    client_secret = $AppSecret
+}
+$response = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token" -Method Post -ContentType "application/x-www-form-urlencoded" -Body $body
+$accessToken = $response.access_token
+
+# Register the device to Intune using Get-WindowsAutopilotInfo
+Get-WindowsAutopilotInfo -GroupTag $groupTag -Online -TenantId $tenantId -ClientId $clientId -ClientSecret $clientSecret
+
+# Output the result
+Write-Output "Device has been registered to Intune with group tag $groupTag."
